81f945d999de94aaa0b075146c60dcdd939b908e
Jeff Kerpan <jkerpan@gmail.com>
2017-05-25 15:34:53 -0600

    Passing all Express Server tests

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+
 // const petsFile = fs.readFile('pets.json');
 
 
 app.get('/pets', function(req, res) {
   fs.readFile('./pets.json', 'utf8', function(err, data) {
-    res.send(data);
+    var parsed = JSON.parse(data);
+    res.send(parsed);
+  });
+});
+
+app.get('/pets/:num', function(req, res, next) {
+  fs.readFile('./pets.json', 'utf8', function(err, data) {
+    var parsed = JSON.parse(data);
+    if (req.params.num < parsed.length && req.params.num >= 0) {
+      // console.log('MADE TO IF');
+      // console.log(parsed);
+      res.send(parsed[req.params.num]);
+    } else {
+      res.sendStatus(404);
+    }
   });
 });
 
@@ -14,6 +29,12 @@ app.get('/', function(req, res) {
   res.send('Hello from API');
 });
 
+// app.use((req, res, next) => {
+//   res.status(404).send('Not Found');
+// });
+
 app.listen(8000, function() {
   console.log('Server Listening');
 });
+
+module.exports = app;
